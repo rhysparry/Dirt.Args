3fabb2341658a935fbdcd5261a36214d0714865a
Rhys Parry <rhys@i-think22.net>
2024-03-23 11:41:41 +1000

    feat: Add support for netstandard2.0
    Resolves: #3

--- Dirt.Args/FlagData.cs
+++ Dirt.Args/FlagData.cs
@@ -4,8 +4,13 @@ internal class FlagData(string flag) : IFlagData
 {
     public FlagData(string flag, string value) : this(flag) => Values = new[] { value };
     public string Flag { get; } = flag;
+#if NETSTANDARD2_0
+    public int Count { get; private set; } = 1;
+    public IReadOnlyList<string> Values { get; private set; } = [];
+#else
     public int Count { get; private init; } = 1;
     public IReadOnlyList<string> Values { get; private init; } = [];
+#endif
 
     public FlagData AddValue(string value) => new(Flag)
     {
