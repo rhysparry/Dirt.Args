568976c5d00e0b597427addc7aae5cde6e0c47be
Rhys Parry <rhys@i-think22.net>
2025-04-29 08:33:33 +1000

    fix: update formatted files for csharpier
    - Update the pre-commit config to format files
    - Update files to match new conventions

--- build/Build.cs
+++ build/Build.cs
@@ -14,7 +14,6 @@ class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main() => Execute<Build>(x => x.Compile);
 
     AbsolutePath Artifacts => RootDirectory / "artifacts";
